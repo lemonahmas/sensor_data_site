f805ed5af36206fb4ce85ca2ad2c4d2be910b7ee
lllzm <lemonahmas@foxmail.com>
2021-01-24 18:17:00 +0800

    update

--- sensor_data_handlers.py
+++ sensor_data_handlers.py
@@ -1,1 +1,14 @@
-import flask
+from flask import Flask
+import os
+
+from flask.helpers import get_debug_flag
+
+app = Flask("sensor_data")
+app.debug = True
+@app.route("/")
+def main_page():
+    app_name = app.import_name
+    return f"Hello! This site is {app_name}."
+
+if __name__ == "__main__":
+    app.run()
